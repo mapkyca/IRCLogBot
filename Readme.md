d1f7c81ece0621c874aacba4fc54b2ba4c2f7ef8
Marcus Povey <marcus@marcus-povey.co.uk>
2015-07-19 19:11:46 +0100

    Initial rough version, still needs message parsing

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# IRC Logger Bot
+
+This is a very simple bot which connects to a given IRC channel and creates markdown formatted logs.
+
+I wrote this as a stopgap until Loki was set up on #knownchat, but figured it might be useful to others
+
+## Usage
+
+./bot -s server -p port -c channel -n nick -d logdirectory -a nickservpass
+
+
---
+++ bot.py
@@ -0,0 +1,145 @@
+#!/usr/bin/python
+###
+# Simple IRC Log bot
+###
+
+import signal
+import socket
+import ssl
+import sys
+import time
+import getopt
+import os
+import errno
+
+server = "irc.freenode.net"
+port = 6697
+channel = ""
+botnick = "mapkycalog"
+logdir = "logs"
+password = ""
+
+irc_C = None
+irc = None
+
+def logline(line, logdir):
+    filename = time.strftime("%Y-%m-%d") + ".md"
+
+    # Parse out string and format appropriately    
+    
+    with open(logdir + "/" + filename, "a") as logfile:
+        logfile.write(line + "\n")
+        logfile.close()
+        
+
+def sig_int_handler(signal, frame):
+    print("SIGINT: Shutting down nicely...")
+    irc.send("QUIT (Botdeath)\r\n")
+    sys.exit()
+
+def send(command):
+    global irc
+    print "[SENT] " + command
+    irc.send(command)
+    
+def usage():
+    print("IRC Log bot by Marcus Povey <marcus@marcus-povey.co.uk>");
+    print;
+    print("Usage:");
+    print("\t./bot -s server -p port -c channel -n nick -d logdirectory -a nickservpass");
+
+def runbot(server, port, channel, botnick, logdir, password):
+    global irc_C, irc
+    
+    irc_C = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #defines the socket
+    irc = ssl.wrap_socket(irc_C)
+    
+    print "Establishing connection to %s on %s:%d" % (channel, server, port)
+    print "Logging to %s" % (logdir)
+    
+    # Connect
+    irc.connect((server, port))
+    irc.setblocking(False)
+    send("USER " + botnick + " " + botnick + " bla :" + botnick + "\r\n")
+    send("NICK " + botnick + "\r\n")
+    if len(password) > 0:
+        send("PRIVMSG nickserv :" + password + "\r\n")    #auth
+    send("JOIN " + channel + "\r\n")
+    
+    while True:
+        time.sleep(1)
+        	    
+        try:
+            text=irc.recv(2040)
+            print "[RECEIVED] " + text
+        
+            # Prevent Timeout
+            if text.find('PING') != -1:
+                send('PONG ' + text.split() [1] + '\r\n')
+                
+            # Someone on IRC said something on the channel, so log it, and mayby parse it for meaning
+            if text.find('PRIVMSG '+ channel) != -1:
+                logline(text, logdir)
+        	    
+        	    
+        except Exception:
+            continue
+
+def main():
+    global server
+    global port
+    global channel
+    global botnick
+    global logdir
+    global password
+    
+    signal.signal(signal.SIGINT, sig_int_handler)
+    
+    try:
+		opts, args = getopt.getopt(sys.argv[1:], "s:p:c:n:d:h", ["help"])
+    except getopt.GetoptError, err:
+		print str(err)
+		usage()
+		sys.exit()
+
+    for o, a in opts:
+        if o == "-s":
+            server = a
+        elif o == "-p":
+            port = int(a)
+        elif o == "-c":
+            channel = a
+        elif o == "-n":
+            botnick = a
+        elif o == "-d":
+            logdir = a
+        elif o == "-a":
+            password = a
+        elif o in ("-h", "--help"):
+            usage()
+            sys.exit()
+        else:
+            usage()
+            sys.exit()    
+            
+    # Check channel is defined
+    if len(channel) == 0:
+        usage()
+        sys.exit()
+
+    # Normalise log path and create dirs
+    logdir = os.path.abspath(logdir) + "/" + channel
+    try:
+        os.makedirs(logdir)
+    except OSError as exc: # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(logdir):
+            pass
+        else: raise
+    
+    # Can't put # on command line, so add it
+    channel = "#" + channel
+    
+    runbot(server, port, channel, botnick, logdir, password)
+
+if __name__ == "__main__":
+    main()
